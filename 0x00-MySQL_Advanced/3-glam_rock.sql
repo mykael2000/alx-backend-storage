d96517d52522d80ead1003d98aacfdbd0cac330a
Michael Erameh <eramehmichael2000@gmail.com>
2023-08-12 10:17:43 +0100

    change

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Old school band
+-- SELECT band_nme
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
